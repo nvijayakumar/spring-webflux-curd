93e8c5597856cc7fc20eb9a8e071c3bff93429c8
nvijayakumar <vijay.be.kumar@gmail.com>
2022-11-02 06:46:33 +0530

    first commit

---
+++ src/main/java/org/vj/app/SpringWebfluxCurdApplication.java
@@ -0,0 +1,13 @@
+package org.vj.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxCurdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/vj/app/SpringWebfluxCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vj.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
